ff8196d6ecfa36c4c56a3748aad711bfda36de66
Young-abc <3219337115@qq.com>
2026-04-17 10:40:11 +0800

    Update server.js

--- api/server.js
+++ api/server.js
@@ -3,8 +3,15 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+// 关键：禁用写入操作
+router.render = (req, res) => {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+  res.jsonp(res.locals.data);
+};
+
 server.use(middlewares);
-server.use(jsonServer.bodyParser); // 支持 POST
 server.use(router);
 
 module.exports = server;
